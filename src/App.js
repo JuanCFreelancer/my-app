cdd94125ee1c2031096cce7cc31df47792be9115
JuanCFreelancer <jcentenovillalobos@gmail.com>
2023-01-13 19:47:39 -0500

    format Document

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-        <h1>Mi primera aplicación</h1>
-        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+          <h1>Mi primera aplicación</h1>
+          <p>Esta es mi primera aplicación en React, está padrísimo!</p>
         </div>
       </header>
     </div>
